3284ea65c42e0d5ffd4ed6b39b416b44e920c4a9
Tri Hieu <phamgiatrihieu@gmail.com>
2023-06-26 08:28:09 +0700

    java week1

--- Week1/Day_04/Exercises_Collection/Bai_01.java
+++ Week1/Day_04/Exercises_Collection/Bai_01.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Bai_01 {
     public static void main(String[] args) {
@@ -24,7 +25,12 @@ public class Bai_01 {
             System.out.println("5. Tạo một HashMap chứa các tên và điểm số của sinh viên và in ra tất cả các mục.");
             System.out.println(
                     "6. Viết một phương thức để kiểm tra xem một ArrayList có chứa một giá trị cụ thể hay không.");
+            System.out
+                    .println("9. Viết một chương trình để thêm tất cả các phần tử của một ArrayList vào một HashSet.");
 
+            System.out.println("10. Xóa các phần tử trùng lặp từ một ArrayList.");
+            System.out.println("11. Viết một phương thức để sao chép một ArrayList sang một ArrayList mới.");
+            System.out.println("14. Kiểm tra xem một ArrayList có rỗng hay không.");
             luaChon = sc.nextInt();
             switch (luaChon) {
                 case 1:
@@ -43,6 +49,19 @@ public class Bai_01 {
                 case 6:
                     System.out.println("case 6");
 
+                case 9:
+                    System.out.println("case 9");
+                    addElsIntoHashSetArr();
+                    break;
+                case 10:
+                    System.out.println("case 10");
+                    deleteElsDuplicate();
+                    break;
+                case 11:
+                    System.out.println("11");
+                    copyArr();
+                case 14:
+                    System.out.println("case 14");
                 default:
                     break;
             }
@@ -174,5 +193,90 @@ public class Bai_01 {
     public static void isSpecificValue() {
         ArrayList<Integer> arr = new ArrayList<>();
     }
+
+    // 9. Viết một chương trình để thêm tất cả các phần tử của một ArrayList vào một
+    // HashSet.
+    public static void addElsIntoHashSetArr() {
+        // Create an ArrList
+        ArrayList<Integer> arrayListNumbers = new ArrayList<>();
+
+        // add els into arrayListNumbers
+        arrayListNumbers.add(10);
+        arrayListNumbers.add(20);
+        arrayListNumbers.add(30);
+        arrayListNumbers.add(10);
+
+        System.out.println("ArrayList: " + arrayListNumbers);
+
+        // Create an hashSet arr
+        HashSet<Integer> arrHashSetNumbers = new HashSet<>();
+
+        // add all els from ArrayList into ArrHashSet
+        arrHashSetNumbers.addAll(arrayListNumbers);
+        System.out.println("arrHashSet: " + arrHashSetNumbers);
+        // NOTE hashMap IS NOT allowed to add duplicate els
+
+    }
+
+    // 10. Xóa các phần tử trùng lặp từ một ArrayList.
+    public static void deleteElsDuplicate() {
+        // Create an ArrayList
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // Add els into arr
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(20); // duplicate
+        numbers.add(10); // duplicate
+        numbers.add(50);
+        System.out.println("ArrayList: " + numbers);
+
+        // create numberHashMap arr
+        Set<Integer> numberHashMap = new HashSet<>();
+
+        // using numberHashMap to reject duplicate els
+        numberHashMap.addAll(numbers);
+        System.out.println("numberHashMap: " + numberHashMap);
+
+        // clearing numbers arr
+        numbers.clear();
+        System.out.println("ArrayList after clearing: " + numbers);
+
+        // add els from numberHashMap to numbers arr
+        numbers.addAll(numberHashMap);
+
+        // Sort els by Increasing
+        Collections.sort(numbers);
+        System.out.println("Delete duplicates els: " + numbers);
+
+    }
+
+    // 11. Viết một phương thức để sao chép một ArrayList sang một ArrayList mới.
+    public static void copyArr() {
+        // Create an Arr
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // add els into numbersArr
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(10); // duplicate
+        numbers.add(50);
+        System.out.println("ArrayList: " + numbers);
+
+        // Create a New Array
+        // Object clone()
+        ArrayList<Integer> copyNumbersArr = new ArrayList<>();
+        System.out.println("Origin copyNumbersArr: " + copyNumbersArr);
+
+        copyNumbersArr = (ArrayList<Integer>) numbers.clone();
+
+        System.out.println("Updated copyNumbersArr after clone from numbersArr: " + copyNumbersArr);
+
+    }
+
+    // 14. Kiểm tra xem một ArrayList có rỗng hay không.
 
 }
